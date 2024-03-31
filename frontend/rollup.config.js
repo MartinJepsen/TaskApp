02b669c6b07d390c1d00f9ffe28e72d36ad3ff6a
Martin Jepsen <martinjepsen@proton.me>
2024-03-31 16:01:28 +0200

    add typescript config

---
+++ frontend/rollup.config.js
@@ -0,0 +1,29 @@
+import rollup_nre from "@rollup/plugin-node-resolve";
+import rollup_tsc from "rollup-plugin-typescript2";
+
+export default [
+    {
+        input: "src/main.ts",
+        output: {
+            file: "dist/js/app-bundle.js",
+            format: "iife",
+            name: "bundle",
+            sourcemap: true
+        },
+        plugins: [
+            rollup_nre(),
+            rollup_tsc({
+                verbosity: 1,
+                tsconfig: "tsconfig.json",
+                tsconfigOverride: {
+                    compilerOptions: {
+                        declaration: false,
+                        declarationMap: false,
+                        sourceMap: true
+                    }
+                },
+                useTsconfigDeclarationDir: false
+            })
+        ]
+    }
+]
